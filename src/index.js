dd7653718c49350573dfecd3ad60b8ffd9feb690
LiLQD <luuquangdung1511@gmail.com>
2026-06-17 17:23:52 +0700

    feat: implement fibonanci functions that return array

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-import './styles.css';
+const fib = function (n) {
+  const arr = [0];
+  for (let i = 1; i < n; i++) {
+    const first = arr[i - 2] || 0;
+    const second = arr[i - 1] || 1;
+    arr[i] = first + second;
+  }
+  return arr;
+};
 
-console.log('Test Connection');
+const fibsRec = function (n) {
+  if (n <= 0) return [];
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
+  const arr = fibsRec(n - 1);
+  const next = arr[arr.length - 1] + arr[arr.length - 2];
+  arr.push(next);
+  return arr;
+};
+console.log(fib(8));
+console.log(fibsRec(8));

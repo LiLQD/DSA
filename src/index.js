778e8f12cf31b2f6f6e1aa994b85182812890100
LiLQD <luuquangdung1511@gmail.com>
2026-06-17 18:21:21 +0700

    feat: implement mergeSort function

--- src/index.js
+++ src/index.js
@@ -17,5 +17,20 @@ const fibsRec = function (n) {
   arr.push(next);
   return arr;
 };
+
+const mergeSort = function (arr) {
+  if (arr.length <= 1) return arr;
+  const middle = Math.floor(arr.length / 2);
+  const leftArr = mergeSort(arr.slice(0, middle));
+  const rightArr = mergeSort(arr.slice(middle));
+  const result = [];
+  while (leftArr.length > 0 && rightArr.length > 0) {
+    const left = leftArr[0];
+    const right = rightArr[0];
+    left < right ? result.push(leftArr.shift()) : result.push(rightArr.shift());
+  }
+  return result.concat(leftArr, rightArr);
+};
 console.log(fib(8));
 console.log(fibsRec(8));
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 10, 1]));

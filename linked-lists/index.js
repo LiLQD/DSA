65b9940416252196374aeb87b0a8c916b7769fce
LiLQD <luuquangdung1511@gmail.com>
2026-06-23 11:51:24 +0700

    feat: implement linked list and node class

---
+++ linked-lists/index.js
@@ -0,0 +1,13 @@
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+}
+class Node {
+  value = null;
+  nextNode = null;
+  constructor(value, node) {
+    this.value = value;
+    this.nextNode = node;
+  }
+}

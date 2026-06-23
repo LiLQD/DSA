54d3718d51a93a2b3b518c01a5ab26bd64de4cd9
LiLQD <luuquangdung1511@gmail.com>
2026-06-23 11:52:50 +0700

    feat: implement append method

--- linked-lists/index.js
+++ linked-lists/index.js
@@ -2,6 +2,19 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
+  append(value) {
+    const newNode = new Node(value, null);
+    if (this.head === null) {
+      this.head = newNode;
+      return;
+    } else {
+      let currentNode = this.head;
+      while (currentNode.nextNode !== null) {
+        currentNode = currentNode.nextNode;
+      }
+      currentNode.nextNode = newNode;
+    }
+  }
 }
 class Node {
   value = null;

4e083574759bb556685019d63cd5538510f51461
LiLQD <luuquangdung1511@gmail.com>
2026-06-25 18:14:32 +0700

    feat: implement indexValid method

--- hash-map/index.js
+++ hash-map/index.js
@@ -3,5 +3,9 @@ class HashMap {
     this.loadFactor = loadFactor;
     this.capacity = capacity;
   }
+  indexValid(index) {
+    if (index < 0 || index >= buckets.length)
+      throw new Error('Trying to access index out of bounds');
+  }
 }
 module.exports = { HashMap };

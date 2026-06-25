412212c502692624e21166e035733b61b8e8b1ab
LiLQD <luuquangdung1511@gmail.com>
2026-06-25 18:14:42 +0700

    feat: implement hash method

--- hash-map/index.js
+++ hash-map/index.js
@@ -7,5 +7,13 @@ class HashMap {
     if (index < 0 || index >= buckets.length)
       throw new Error('Trying to access index out of bounds');
   }
+  hash(key) {
+    let hashCode = 0;
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+    }
+    return hashCode;
+  }
 }
 module.exports = { HashMap };

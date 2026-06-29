9d55f4db4726661aafa2237212ffa73409b1a6f7
LiLQD <luuquangdung1511@gmail.com>
2026-06-29 17:22:45 +0700

    feat: implement buildTree method and use it in constructor

--- binary-search-tree/index.js
+++ binary-search-tree/index.js
@@ -27,19 +27,17 @@ const removeDuplicates = (array) => {
 export class Tree {
   constructor(array) {
     this.array = removeDuplicates(mergeSort(array));
-    this.root = null;
+    this.root = this.buildTree(this.array);
   }
   buildTree(array) {
-    console.log(array);
-    const middle = array.length / 2;
     if (array.length <= 0) return null;
-    this.root = new Node(middle);
-    this.root.left = this.buildTree(array.slice(0, middle - 1));
-    this.root.right = this.buildTree(array.slice(middle + 1, array.length));
-    return this.root;
+    const middle = Math.floor(array.length / 2);
+    const newRoot = new Node(array[middle]);
+    newRoot.left = this.buildTree(array.slice(0, middle));
+    newRoot.right = this.buildTree(array.slice(middle + 1, array.length));
+    return newRoot;
   }
 }
 const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 console.log(tree.array);
-// tree.buildTree(tree.array);
-// prettyPrint(tree.root);
+prettyPrint(tree.buildTree(tree.array));
